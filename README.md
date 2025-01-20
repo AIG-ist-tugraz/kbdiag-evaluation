5d7cd6f79ebf61fe0d0196d3c198b21a7aec0615
Lê Viết Mẫn <manleviet@gmail.com>
2025-01-20 21:00:13 +0100

    Done evaluation for hsd

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # KBDiag
+
+## Model-based diagnosis with multiple observations (IJCAI'19)
+
+Step 1 - use cnf2wcnf.py to convert DELL.dimacs, ubuntu.dimacs, and windows8.dimacs to wcnf format.
+The current wcnf files don't have test cases.
+
---
+++ data/ijcai24_25/data/wcnf/cnf2wcnf.py
@@ -0,0 +1,45 @@
+#  KBDiag
+#
+#  Copyright (c) 2025
+#
+#  @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+
+#  KBDiag
+#
+#
+#  @author: Viet-Man Le (vietman.le@ist.tugraz.at)
+from pysat.formula import CNF
+
+KBs = ['DELL', 'ubuntu', 'windows8']
+
+for KB in KBs:
+    fn = '%s.dimacs' % KB
+    print(f'Converting {fn} to wcnf')
+
+    formula = CNF(from_file=fn)
+
+    print(f'\t#variables: {formula.nv}')
+    print(f'\t#clauses: {len(formula.clauses)}')
+
+    wcnf = formula.weighted()
+
+    print('\tAfter weighted')
+    print(f'\t\tHard clauses: {wcnf.hard}')
+    print(f'\t\t#Hard clauses: {len(wcnf.hard)}')
+    print(f'\t\t#Soft clauses: {len(wcnf.soft)}')
+    print(f'\t\t#Weights: {len(wcnf.wght)}')
+
+    # remove the first element from the soft clauses
+    # add the first element to the hard clauses
+    wcnf.hard.append(wcnf.soft.pop(0))
+    wcnf.wght.pop(0)
+
+    print('\tAfter modification')
+    print(f'\t\tHard clauses: {wcnf.hard}')
+    print(f'\t\t#Hard clauses: {len(wcnf.hard)}')
+    print(f'\t\t#Soft clauses: {len(wcnf.soft)}')
+    print(f'\t\t#Weights: {len(wcnf.wght)}')
+
+    wcnf.to_file('%s.wcnf' % KB)
+
+print('Done')
